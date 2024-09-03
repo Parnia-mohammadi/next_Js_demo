864835f95d9f5eb72d8f9ee1ec17c6775ae99e28
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2024-09-03 14:46:07 +0330

    show params in catch all routes(shop segment)

--- src/app/shop/[[...slug]]/page.js
+++ src/app/shop/[[...slug]]/page.js
@@ -1,6 +1,11 @@
 function ShopPage({ params }) {
-  console.log(params);
-  return <div>ShopPage</div>;
+  // console.log(params);
+  return (
+    <div>
+      we are in shop page
+      {JSON.stringify(params) == "{}" ? "" : "/" + params.slug.join("/")}
+    </div>
+  );
 }
 
 export default ShopPage;
