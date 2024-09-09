e050511da71198b101529d73a2f8a440825bafd0
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2024-09-09 17:01:20 +0330

    dynamic metadata (generatemetadata f())added.

--- src/app/(admin)/products/[productId]/page.js
+++ src/app/(admin)/products/[productId]/page.js
@@ -1,3 +1,18 @@
+import { resolve } from "styled-jsx/css";
+
+export async function generateMetadata({ params }) {
+  const id = params.productId;
+  const title = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`T-shirt-${id}`);
+    }, 1000);
+  });
+  return {
+    title: title,
+    description: "products of page.",
+  };
+}
+
 function ProductDetails({ params }) {
   //   console.log(params);
   return <div>ProductDetails-{params.productId}</div>;
